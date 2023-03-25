e5858dc9ca96a349e0f257083afa0d1ad767056b
Shivanandkkhh <Shivanand@shoptrade.co>
2023-03-25 16:37:40 +0530

    big fixes

--- assets/custom.js
+++ assets/custom.js
@@ -5,11 +5,3 @@
   btn.addEventListener('click', function() {
     panel.classList.toggle('show');
   });
- 
-//   var link_handle = false;
-//   const dataObject = {};
-//   dataObject [{{ link_handle }}] = false; 
-  
-//   const element = document.querySelector('[x-data]');
-//   element.setAttribute('x-data', JSON.stringify(dataObject));
-  
