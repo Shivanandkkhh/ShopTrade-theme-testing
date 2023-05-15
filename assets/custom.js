9dc22a9d5fa54c91c4d1b8aecb93ed3352191604
Shivanandkkhh <Shivanand@shoptrade.co>
2023-05-15 16:11:22 +0530

    review section related product's section bug fixes

--- assets/custom.js
+++ assets/custom.js
@@ -43,32 +43,14 @@ $(document).ready(function() {
     }
   );
 });
-let btnm = document.getElementsByClassName("btn-pg-bar")[0];
-btnm.addEventListener("click", function () {
-    var ele = document.querySelectorAll(".discount_bar")[0];
-    var svg = document.querySelectorAll(".discount_bar_circle")[0];
-    var width = 1;
-    var margin = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id)
-        }
-        else {
-            width++;
-            ele.style.width = width + '%';
-        }
-    }
-    var iid = setInterval(frames, 10);
-    function frames() {
-        if (margin >= 100) {
-            clearInterval(iid)
-        }
-        else {
-            margin++;
-            svg.style.marginLeft = margin + '%';
-        }
-    }
-});
+var ele = document.querySelectorAll(".discount_bar")[0];
+var svg = document.querySelectorAll(".discount_bar_circle")[0];
+var totalPrice = JSON.parse(ele.getAttribute("data-total-price"));
+var percentage = (totalPrice / 500000) * 100;
+if (percentage > 100){
+  percentage = 100;
+}
+svg.style.marginLeft = percentage + '%';
+ele.style.width = percentage + '%';
 
 
