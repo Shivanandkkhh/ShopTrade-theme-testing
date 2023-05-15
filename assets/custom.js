36f0fc6abbf9b5dbff95323e59db3724a1f942a7
Shivanandkkhh <Shivanand@shoptrade.co>
2023-05-15 13:30:40 +0530

    related products sections added and star ratings added

--- assets/custom.js
+++ assets/custom.js
@@ -1,74 +1,64 @@
 
-  const btn = document.querySelector('.btn');
-  const panel = document.querySelector('#panel');
+const btn = document.querySelector('.btn');
+const panel = document.querySelector('#panel');
 
-  btn.addEventListener('click', function() {
-    panel.classList.toggle('show');
-  });
- 
+btn.addEventListener('click', function () {
+  panel.classList.toggle('show');
+});
+
 //   var link_handle = false;
 //   const dataObject = {};
 //   dataObject [{{ link_handle }}] = false; 
-  
+
 //   const element = document.querySelector('[x-data]');
 //   element.setAttribute('x-data', JSON.stringify(dataObject));
-  
+
 
 // cart
-const cart  = document.querySelector(".cart");
+const cart = document.querySelector(".cart");
 const cartItems = document.querySelector(".cartItems");
 
-cart.addEventListener('click',function(){
+cart.addEventListener('click', function () {
   cartItems.classList.toggle('show');
 })
 
 // 
-$(document).ready(function() {
+$(document).ready(function () {
   $('.slider-marquee-content-parent').slick({
     // slidesToShow: 2,
     dots: false,
     autoplaySpeed: 0,
     autoplay: true,
-    speed:10000,
+    speed: 10000,
     slidesToScroll: 2,
     cssEase: 'linear',
     infinite: true,
   });
   $('.slider-marquee-content').hover(
-    function() {
+    function () {
       $(this).slick('slickPause');
     },
-    function() {
+    function () {
       $(this).slick('slickPlay');
     }
   );
 });
-let btnm = document.getElementsByClassName("btn-pg-bar")[0];
-btnm.addEventListener("click", function () {
-    var ele = document.querySelectorAll(".discount_bar")[0];
-    var svg = document.querySelectorAll(".discount_bar_circle")[0];
-    var width = 1;
-    var margin = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id)
-        }
-        else {
-            width++;
-            ele.style.width = width + '%';
-        }
-    }
-    var iid = setInterval(frames, 10);
-    function frames() {
-        if (margin >= 100) {
-            clearInterval(iid)
-        }
-        else {
-            margin++;
-            svg.style.marginLeft = margin + '%';
-        }
-    }
-});
+
+
+// the progressive bar at cs ortho product page
+var ele = document.querySelectorAll(".discount_bar")[0];
+var svg = document.querySelectorAll(".discount_bar_circle")[0];
+var totalPrice = JSON.parse(ele.getAttribute("data-total-price"));
+var percentage = (totalPrice / 500000) * 100;
+if (percentage > 100){
+  percentage = 100;
+}
+svg.style.marginLeft = percentage + '%';
+ele.style.width = percentage + '%';
+
+
+
+
+
 
 
